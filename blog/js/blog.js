23fcffe69bd11eb07b4f6ee91dd812ab5cc0a2f5
= <=>
2022-05-26 18:35:23 +0100

    Modified resizing mechanism to account for larger screens / encapsulated resizing mechanism in a single function

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function () {
     const SOCIAL_MEDIA_CONTAINER = document.getElementById('social-media-container');
     const BLOGGER_NAME = document.getElementById('blogger-name');
     const NAVIGATION = document.getElementsByTagName('nav')[0];
+    const FOOTER = document.getElementsByTagName('footer')[0];
 
     const STYLESHEET_RULES = document.styleSheets[0].cssRules;
     const CUSTOM_FONT_CSS = STYLESHEET_RULES[3].style;
@@ -11,41 +12,65 @@ window.addEventListener('load', function () {
 
 
     // RESIZING
-    let referenceRect = SOCIAL_MEDIA_CONTAINER.getBoundingClientRect();
+    let sectionSelected = 'mylife';
 
-    __resizeProfilePicture__(referenceRect);
-    __resizeName__(referenceRect);
-    __resizeNavigationText__(referenceRect);
-    __resizeAboutMeComponents__(referenceRect);
+    __resizeActiveComponents__();
+    window.addEventListener('resize', __resizeActiveComponents__);
 
-    window.addEventListener('resize', function () {
-        referenceRect = SOCIAL_MEDIA_CONTAINER.getBoundingClientRect();
+    function __resizeActiveComponents__() {
+        let referenceRect = SOCIAL_MEDIA_CONTAINER.getBoundingClientRect();
+        const WINDOW_WIDTH = window.innerWidth;
 
-        __resizeProfilePicture__(referenceRect);
-        __resizeName__(referenceRect);
-        __resizeNavigationText__(referenceRect);
-        __resizeAboutMeComponents__(referenceRect);
-    });
+        __resizeFixedComponents__(referenceRect, WINDOW_WIDTH);
 
-    function __resizeProfilePicture__(referenceRect) {
+        switch (sectionSelected) {
+            case 'about':
+                __resizeAboutMeComponents__(referenceRect, WINDOW_WIDTH);
+                break;
+            case 'mylife':
+                break;
+            case 'gallery-art':
+
+        }
+    };
+
+    function __resizeFixedComponents__(referenceRect, windowWidth) {
         const PFP_STYLES = PROFILE_PICTURE.style;
-        const NEW_SIZE = Math.round(referenceRect.width * 0.6) + 'px';
 
+        let pfp_multiplier = 0.6;
+        let name_multiplier = 1.2;
+        let navigationText_multiplier = 0.5;
+        let footerText_multiplier = 0.4;
+
+        if (windowWidth > 1200) {
+            pfp_multiplier = 0.4;
+            name_multiplier = 0.7;
+            navigationText_multiplier = 0.35;
+            footerText_multiplier = 0.25;
+        }
+
+        const NEW_SIZE = Math.round(referenceRect.width * pfp_multiplier) + 'px';
         PFP_STYLES.width = NEW_SIZE;
         PFP_STYLES.height = NEW_SIZE;
+
+        BLOGGER_NAME.style.fontSize = (referenceRect.height * name_multiplier) + 'px';
+
+        NAVIGATION.style.fontSize = (referenceRect.height * navigationText_multiplier) + 'px';
+
+        FOOTER.style.fontSize = (referenceRect.height * footerText_multiplier) + 'px';
     };
 
-    function __resizeName__(referenceRect) {
-        BLOGGER_NAME.style.fontSize = (referenceRect.height * 1.2) + 'px';
-    };
+    function __resizeAboutMeComponents__(referenceRect, windowWidth) {
+        let header_multiplier = 1;
+        let paragraph_multiplier = 0.43;
 
-    function __resizeNavigationText__(referenceRect) {
-        NAVIGATION.style.fontSize = (referenceRect.height * 0.5) + 'px';
-    };
+        if (windowWidth > 1200) {
+            header_multiplier = 0.5;
+            paragraph_multiplier = 0.3;
+        }
 
-    function __resizeAboutMeComponents__(referenceRect) {
-        CUSTOM_FONT_CSS.fontSize = referenceRect.height + 'px';
-        SECTION_PARAGRAPHS_CSS.fontSize = (referenceRect.height * 0.43) + 'px';
+        CUSTOM_FONT_CSS.fontSize = (referenceRect.height * header_multiplier) + 'px';
+        SECTION_PARAGRAPHS_CSS.fontSize = (referenceRect.height * paragraph_multiplier) + 'px';
     };
 
 
@@ -68,7 +93,9 @@ window.addEventListener('load', function () {
             ELEMENT.classList.add('selected');
 
             // displays selected section
-            document.getElementById(ELEMENT.getAttribute('data-section')).classList.remove('hide');
+            sectionSelected = ELEMENT.getAttribute('data-section');
+            document.getElementById(sectionSelected).classList.remove('hide');
+            __resizeActiveComponents__();
 
             // hides previously selected section
             document.getElementById(currentlySelectedElement.getAttribute('data-section')).classList.add('hide');
